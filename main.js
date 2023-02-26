a48f046df119ec84b4e208ffc04f581d2b4a6dde
Mohamed Chellott <chellouttmohamed@gmail.com>
2023-02-26 13:33:59 +0100

    26-02

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const clickbtn = document.getElementById("btn");
+const show = document.getElementById("show");
 const translated = document.getElementById("translated");
 const quotes = document.getElementById("quote");
 const author = document.getElementById("author");
 
-const ApiKey = 'ee893cae65msh738d38a82c2f60ep17b9fajsn8f0dafaeef02';
-
+const ApiKey = "ee893cae65msh738d38a82c2f60ep17b9fajsn8f0dafaeef02";
 
 const options = {
   method: "GET",
@@ -15,50 +15,40 @@ const options = {
 const ApiUrl =
   "https://quotes15.p.rapidapi.com/quotes/random/?language_code=de";
 
-
-
 async function getQoute() {
-     
-    quotes.innerText = "getting quotes...";
-    const response = await fetch(ApiUrl, options);
-    const data = await response.json();
-    quotes.innerText = `"${data.content}"`;
-    author.innerText = `-${data.originator.name}`;
-  
+  quotes.innerText = "getting quotes...";
+  author.innerText = "...";
+  const response = await fetch(ApiUrl, options);
+  const data = await response.json();
+  quotes.innerText = `"${data.content}"`;
+  author.innerText = `-${data.originator.name}`;
+  translated.innerText = "getting quotes...";
+  translated.style.visibility = "hidden";
 
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("from", "de");
-    encodedParams.append("to", "ar");
-    encodedParams.append("text", data.content);
-    
-    const options2 = {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'X-RapidAPI-Key': 'ee893cae65msh738d38a82c2f60ep17b9fajsn8f0dafaeef02',
-        'X-RapidAPI-Host': 'translo.p.rapidapi.com'
-      },
-      body: encodedParams
-    };
-    
-    fetch('https://translo.p.rapidapi.com/api/v3/translate', options2)
-    .then(response => response.json())
-    .then(response =>  translated.innerText =   (response.translated_text))
-    .catch(err => console.error(err));
-  
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("from", "de");
+  encodedParams.append("to", "ar");
+  encodedParams.append("text", data.content);
+
+  const options2 = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "ee893cae65msh738d38a82c2f60ep17b9fajsn8f0dafaeef02",
+      "X-RapidAPI-Host": "translo.p.rapidapi.com",
+    },
+    body: encodedParams,
+  };
+
+  fetch("https://translo.p.rapidapi.com/api/v3/translate", options2)
+    .then((response) => response.json())
+    .then((response) => (translated.innerText = response.translated_text))
+    .catch((err) => console.error(err));
 }
 
-
+function showTranslate() {
+  translated.style.visibility = "visible";
+}
 
 clickbtn.addEventListener("click", getQoute);
-
-
-
-
-
-
-
-
-
-
-
+show.addEventListener("click", showTranslate);
